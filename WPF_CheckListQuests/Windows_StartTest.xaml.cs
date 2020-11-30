0ca3b35625000bc29041a1a6b1bd17931947fba5
Георгий Исаев <georgiyelbaf@gmail.com>
2020-11-30 16:12:01 +0300

    Окно для теста, сделано!

--- WPF_CheckListQuests/Windows_StartTest.xaml.cs
+++ WPF_CheckListQuests/Windows_StartTest.xaml.cs
@@ -20,7 +20,7 @@ namespace WPF_CheckListQuests
         public Windows_StartTest()
         {
             InitializeComponent();
-            TextBox_Quest.Text = "efwwwwwwwwwwwwwwwwwwwwwwwwwwwww  eefwfwfwfwfwfwfwfwfwfwfwfwfw ewf wefwe fwe wefewf ewfew fewf ewf ewfwefew fewfwefewf ef ew e fwef ew ffeew f wefew few we few fwe fewf wef ewf wefwe ew e we fwef";
+            TextBox_Quest.Text = "efwwwwwwwwwwwwwwwwwwwwwwwwwwwwrge sdsg sdfg sdfsdf sdf sdf sdf sdf sdf wwwwwwwwwwwwwww w wwwwwwwwwwwwwww w  wwwwwwwwwwwwwwwww   wwwwwwwwwwwwwwwwwwwwww  eefwfwfwfwfwfwfwfwfwfwfwfwfw ewf wefwe fwe wefewf ewfew fewf ewf ewfwefew fewfwefewf ef ew e fwef ew ffeew f wefew few we few fwe fewf wef ewf wefwe ew e we fwef";
           
         }
     }
