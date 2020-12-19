9a5db2d1c2868ffd13186ac2cbdc898870aefa5b
Георгий Исаев <georgiyelbaf@gmail.com>
2020-12-20 00:39:24 +0300

    Добавлено описание приложения в окно описания, добавлено оформление для описания

--- WPF_CheckListQuests/Window_DescriptionForApplication.xaml.cs
+++ WPF_CheckListQuests/Window_DescriptionForApplication.xaml.cs
@@ -20,6 +20,20 @@ namespace WPF_CheckListQuests
         public Window_DescriptionForApplication()
         {
             InitializeComponent();
+            Loaded += Window_DescriptionForApplication_Loaded;
+        }
+
+        private void Window_DescriptionForApplication_Loaded(object sender, RoutedEventArgs e)
+        {
+            TextBlock_info.Text= "Предназначение приложения:\n\n" +
+                "1. Приложение позволяет собирать вопросы и ответы в единый чек-лист в виде HTML-страницы \n"+
+                "2. Приложение проверяет добавляемые вопрос на повторы\n"+
+                "3. Приложение позволяет добавлять к вопросу краткое пояснение\n"+
+                "4. Сохранённые HTML-чек-листы можно редактировать через приложение\n"+
+                "5. Сохранённые HTML-чек-листы можно использовать для запуска активных тестов внутри приложения для проверки своих знаний";
+
+            TextBlock_sign.Text = " Made by georgiyelbaf\n  Copyright © 2020";
+
         }
     }
 }
