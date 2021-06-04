e66653cd320c02b4e32b0ca97c1287a6b52e6efd
Георгий Исаев <georgiyelbaf@gmail.com>
2021-06-04 23:37:19 +0300

    add  struct StructCCS

--- GI.CS.WPF.Core.CheckList/StructCCS.cs
+++ GI.CS.WPF.Core.CheckList/StructCCS.cs
@@ -4,7 +4,32 @@ using System.Text;
 
 namespace GI.CS.WPF.Core.CheckList
 {
-    class StructCCS
+    struct StructCCSFont
     {
+        int sizeFront;
+        bool italics;
+        bool bold;
+        bool strike; //зачеркнут
+        string color;
+
+    }
+    struct StructCCSBackend
+    {
+        string color;
+    }
+
+    struct StructCCS
+    {
+        StructCCSBackend baseBackend;
+        StructCCSBackend mainBackend;
+        StructCCSBackend futterBackend;
+
+        StructCCSFont title;
+        StructCCSFont description;
+        StructCCSFont question;
+        StructCCSFont trueAanswer;
+        StructCCSFont falseAnswer;
+        StructCCSFont comment;
+        StructCCSFont signature;
     }
 }
