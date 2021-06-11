d650f9ab382158aa38d544581fca44a56f3fedb2
Георгий Исаев <georgiyelbaf@gmail.com>
2021-06-11 18:00:09 +0300

    ничего не происходит

--- WPF.Test.ColorDialog/MainWindow.xaml.cs
+++ WPF.Test.ColorDialog/MainWindow.xaml.cs
@@ -30,6 +30,41 @@ namespace WPF.Test.ColorDialog
             InitializeComponent();
         }
 
+        private Color color1;
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void RaisePropertyChanged(string property)
+        {
+            PropertyChangedEventHandler handler = this.PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(property));
 
+            }
+        }
+
+        public Color Color1
+        {
+            get
+            {
+                return this.color1;
+            }
+
+            set
+            {
+                this.color1 = value;
+                this.RaisePropertyChanged("Color1");
+
+
+               // TextBox_Color.Text = Convert.ToString(this.color1);
+            }
+        }
+
+        private void ButtonColorDialog_test1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            TextBox_Color.Text = "13";
+            Color? color = ButtonColorDialog_test1.SelectedColor;
+
+            //TextBox_Color.Text = ButtonColorDialog_test1.SelectedColor.ToString();
+        }
     }
 }
