05d3ac46545a6c296bcbd6443c0ef1c32f602cb5
Георгий Исаев <georgiyelbaf@gmail.com>
2022-03-03 01:04:27 +0300

    Нет подключения

--- Test.DBQuestBoxSQLite/Program.cs
+++ Test.DBQuestBoxSQLite/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using DLLDBQuestBox;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,20 @@ namespace Test.DBQuestBoxSQLite
     {
         static void Main(string[] args)
         {
+            try
+            {
+                using (var cont = new DLLDBQuestBox.Data.MyDbContext())
+                {
+                    Console.WriteLine("Есть контакт!");
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Error");
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
         }
+        
     }
 }
