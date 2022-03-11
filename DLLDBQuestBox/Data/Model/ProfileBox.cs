3fc69dd69a61a5aa0cdc76b2dca9fae35b507002
Георгий Исаев <georgiyelbaf@gmail.com>
2022-03-11 03:00:10 +0300

    Иная модель - Единый профиль и работа с ним, запись в базу через отдлеьный метод только когда это нужно

---
+++ DLLDBQuestBox/Data/Model/ProfileBox.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Model
+{
+    public static class ProfileBox
+    {
+        public static Data.Tables.Profile profile;
+
+        public static Data.Tables.Profile ConnectProfile(String name, String password)
+        {
+            Data.Tables.Profile profile = null;
+            try
+            {
+                using (var cont = new Data.MyDbContext())
+                {
+                    foreach (var p in cont.Profiles)
+                    {
+                        if (p.Name == "Admin")
+                        {
+                            if (p.Password != password)
+                            {
+                                throw new Exception("Неверный пароль");
+                            }
+                            profile = p;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Notifi.NoConnection(ex);
+            }
+            return profile;
+        }
+
+        public static void SaveToDB()
+        {      
+            try
+            {
+                using (var cont = new Data.MyDbContext())
+                {
+                    cont.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                Notifi.NoConnection(ex);
+            }     
+        }
+
+    }
+}
