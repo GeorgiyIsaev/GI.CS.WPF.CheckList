8c7bd17226aa2b5a8f884c190ee6566a4b567208
Георгий Исаев <georgiyelbaf@gmail.com>
2022-03-13 17:35:43 +0300

    RefreshExtensions - обновление объекта на актуальную инфу из БД

--- DLLDBQuestBox/Data/Tables/RefreshExtensions.cs
+++ DLLDBQuestBox/Data/Tables/RefreshExtensions.cs
@@ -14,13 +14,21 @@ namespace Data.Tables
             try
             {
                 using (var cont = new Data.MyDbContext())
-                {                
-                    var sqlWhere = cont.Tests.Where(x => x.ProfileId == profile.Id);
-                    foreach (var temp in sqlWhere)
-                    { 
+                {
+                    var sqlWhereProfile = cont.Tests.Where(x => x.ProfileId == profile.Id);
+                    foreach (var testProfile in sqlWhereProfile)
+                    {
                         //Ни чего добавлять не нужно но для обновления
                         //нужно пройтись по каждому эл-ту
                         //Почему так????
+                        var sqlWhereTeest = cont.Quests.Where(x => x.TestId == testProfile.Id);
+                        foreach (var questTest in sqlWhereTeest)
+                        {
+                            var sqlWhereQuest = cont.Answers.Where(x => x.QuestId == questTest.Id);
+                            foreach (var answerQuest in sqlWhereQuest)
+                            {
+                            }
+                        }
                     }
                 }
             }
@@ -40,13 +48,13 @@ namespace Data.Tables
                 using (var cont = new Data.MyDbContext())
                 {
 
-
-                    var sqlWhere = cont.Quests.Where(x => x.TestId == test.Id);
-                    foreach (var temp in sqlWhere)
+                    var sqlWhereTeest = cont.Quests.Where(x => x.TestId == test.Id);
+                    foreach (var questTest in sqlWhereTeest)
                     {
-                        //Ни чего добавлять не нужно но для обновления
-                        //нужно пройтись по каждому эл-ту
-                        //Почему так????
+                        var sqlWhereQuest = cont.Answers.Where(x => x.QuestId == questTest.Id);
+                        foreach (var answerQuest in sqlWhereQuest)
+                        {                  
+                        }
                     }
                 }
             }
@@ -55,9 +63,24 @@ namespace Data.Tables
                 Data.Model.Notifi.NoConnection(ex);
             }
         }
+        public static void Refresh(this Quest quest)
+        {
+            try
+            {
+                using (var cont = new Data.MyDbContext())
+                {
 
+                    var sqlWhereQuest = cont.Answers.Where(x => x.QuestId == quest.Id);
+                    foreach (var answerQuest in sqlWhereQuest)
+                    {
+                    }
 
-
-
+                }
+            }
+            catch (Exception ex)
+            {
+                Data.Model.Notifi.NoConnection(ex);
+            }
+        }
     }
 }
