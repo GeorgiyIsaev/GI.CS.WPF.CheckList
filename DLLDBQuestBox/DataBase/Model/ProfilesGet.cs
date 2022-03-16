ff4b34cfb20a9576b487a1b79b0dec777e584e53
Георгий Исаев <georgiyelbaf@gmail.com>
2022-03-16 19:54:05 +0300

    Получить список профилей

---
+++ DLLDBQuestBox/DataBase/Model/ProfilesGet.cs
@@ -0,0 +1,38 @@
+﻿using DataBase.Tables;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBase.Model
+{
+    public class ProfilesGet
+    {
+        static List<Profile> profiles;    
+
+        public static void Connect(String name = "")
+        {      
+            try
+            {
+                using (var cont = new DataBase.MyDbContext())
+                {
+                    profiles = new List<Profile>();
+                    foreach (var p in cont.Profiles)
+                    {
+                        profiles.Add(p);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Notifi.NoConnection(ex);
+            }          
+        }
+        public static void EndConnect()
+        {
+            profiles = null;
+        }
+
+    }
+}
