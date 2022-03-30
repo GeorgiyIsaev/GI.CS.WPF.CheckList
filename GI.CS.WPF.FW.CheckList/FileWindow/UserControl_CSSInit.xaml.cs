77769fbc12663866cfda2dfd005d7889cb1c2be7
Георгий Исаев <georgiyelbaf@gmail.com>
2022-03-30 18:56:04 +0300

    Отдельный контрол для стилей

---
+++ GI.CS.WPF.FW.CheckList/FileWindow/UserControl_CSSInit.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GI.CS.WPF.FW.CheckList.FileWindow
+{
+    /// <summary>
+    /// Логика взаимодействия для UserControl_CSSInit.xaml
+    /// </summary>
+    public partial class UserControl_CSSInit : UserControl
+    {
+        public UserControl_CSSInit()
+        {
+            InitializeComponent();
+        }
+    }
+}
