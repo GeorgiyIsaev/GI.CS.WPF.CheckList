2b6d4b8b2b2506397619a39cafbe0b62d2455424
Георгий Исаев <georgiyelbaf@gmail.com>
2022-03-30 19:07:03 +0300

    Добавлены события контрола

--- GI.CS.WPF.FW.CheckList/FileWindow/UserControl_CSSInit.xaml.cs
+++ GI.CS.WPF.FW.CheckList/FileWindow/UserControl_CSSInit.xaml.cs
@@ -23,6 +23,58 @@ namespace GI.CS.WPF.FW.CheckList.FileWindow
         public UserControl_CSSInit()
         {
             InitializeComponent();
+            Loaded += Windows_Loaded;
+
+
+        }
+        private void Windows_Loaded(object sender, RoutedEventArgs e)
+        {
+            CreateComboBox_FontSize(); //запалнение шрифтов
+           // FontStyleInit();
+          //  ColorPickerInitialize();
+        }
+
+
+        private void CreateComboBox_FontSize()
+        {
+            for (int i = 6; i < 42; i += 2)
+            {
+                ComboBox_FontSize.Items.Add(i);
+            }
+            StandartStileButton();
+        }
+        private void StandartStileButton()
+        {
+
+            Button_G.IsChecked = true;
+            Button_Z.IsChecked = true;
+            Button_I.IsChecked = true;
+
+            int baseSize = 5;
+            ComboBox_FontSize.SelectedIndex = baseSize;
+        }
+
+
+
+
+
+
+        /*Нажатие на Ж К З*/
+        private void ButtonClick_CSS(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        /*Выбор шрифта*/
+        private void SelectionChanged_FontSize(object sender, SelectionChangedEventArgs e)
+        {
+          
+        }
+
+        /*Выбор Цвета*/
+        private void ColorPicker_ColorInitialize(object sender, MouseEventArgs e)
+        {
+           
         }
     }
 }
