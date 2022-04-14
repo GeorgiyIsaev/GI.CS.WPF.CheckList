5e388b3a1d1e29ff03380401932b72c6509e74e4
Георгий Исаев <georgiyelbaf@gmail.com>
2022-04-14 19:32:24 +0300

    Проект с попытками копирвоания через контрлС

---
+++ Test.CopyCtrlC/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+
+
+namespace MyTestCopy
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += Window_LoadedKeyPaste;   
+        }
+        private void AddTextBox(string text)
+        {
+            if (TB.Text != "") TB.Text += "\n";
+            TB.Text += text;
+        }
+        public void Window_LoadedKeyPaste(object sender, RoutedEventArgs e)
+        {
+            CopyCtrl.AddTextEvent += AddTextBox;    
+            CopyCtrl.Window_LoadedKeyQ(this);
+        }
+    }
+}
