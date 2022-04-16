df8b63508fff96e75b05668b7c1b75c9b7265ff8
Георгий Исаев <georgiyelbaf@gmail.com>
2022-04-16 19:34:29 +0300

    Файлы добавлены к основному проекту

---
+++ GI.CS.WPF.FW.CheckList/CopyPaste/ConstantKey.cs
@@ -0,0 +1,38 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Interop;
+
+namespace MyTestCopy
+{
+    public static class ConstantKey
+    {
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, uint vk);
+
+
+        //необходимые константы
+        public const int MOD_CONTROL = 0x2;
+        public const int WM_HOTKEY = 0x312;
+        public const uint MOD_NOREPEAT = 0;
+        public const int KEY_C = 67;
+
+        //несколько примеров виртуальных кодов
+        public const uint KEY_1 = 0x31;
+        public const uint KEY_2 = 0x32;
+        public const uint KEY_3 = 0x33;
+        public const uint KEY_5 = 0x34;
+
+        public const uint KEY_Q = 0x51;
+        public const uint KEY_W = 0x57;
+        public const uint KEY_E = 0x45;
+        public const uint KEY_R = 0x52;
+
+    }
+}
---
+++ GI.CS.WPF.FW.CheckList/CopyPaste/CopyCtrl.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows;
+using System.Windows.Interop;
+//Windows.Input.Key до System.Windows.Forms.Keys
+//using System.Windows.Forms.Keys;
+
+namespace MyTestCopy
+{
+    public delegate void AddTextBoxDelegate(String text);
+
+    class CopyCtrl
+    {
+
+        public static AddTextBoxDelegate AddTextEvent;
+
+
+        [DllImport("User32.dll")]
+        private static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        static public extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll")]
+        static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, uint dwExtraInfo);
+
+        private static void SendCtrlC(IntPtr hWnd)
+        {
+            uint KEYEVENTF_KEYUP = 2;
+            byte VK_CONTROL = 0x11;
+            SetForegroundWindow(hWnd);
+            keybd_event(VK_CONTROL, 0, 0, 0);
+            keybd_event(0x43, 0, 0, 0); //Send the C key (43 is "C")
+            keybd_event(0x43, 0, KEYEVENTF_KEYUP, 0);
+            keybd_event(VK_CONTROL, 0, KEYEVENTF_KEYUP, 0);// 'Left Control Up
+        }
+
+        //обработчик сообщений для окна
+        private static IntPtr WndProcCopy(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            if (msg == ConstantKey.WM_HOTKEY)
+            {
+                SendCtrlC(hwnd);
+                //System.Windows.Forms.SendKeys.SendWait("^c");       
+                handled = true;
+            }
+            return IntPtr.Zero;
+        }
+
+        //обработчик сообщений для окна
+        private static IntPtr WndProcQ(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            if (msg == ConstantKey.WM_HOTKEY)
+            {    
+                Thread.Sleep(900);
+                AddTextEvent?.Invoke(Clipboard.GetText()); //вызвать вставку из буфера
+                handled = true;
+            }
+            return IntPtr.Zero;
+        }
+
+        /*Метод который добавляет в приложение для окна вызов горячих клавиш*/
+        public static void Window_LoadedKeyQ(Window window)
+        {
+
+            /*Вставка*/
+            WindowInteropHelper h = new System.Windows.Interop.WindowInteropHelper(window);
+            System.Windows.Interop.HwndSource source = System.Windows.Interop.HwndSource.FromHwnd(h.Handle);
+            source.AddHook(new System.Windows.Interop.HwndSourceHook(WndProcQ));//регистрируем обработчик сообщений       
+            bool res = ConstantKey.RegisterHotKey(h.Handle, 1, ConstantKey.MOD_CONTROL | ConstantKey.MOD_NOREPEAT, ConstantKey.KEY_Q);
+            if (res == false) MessageBox.Show("RegisterHotKey failed re2s");
+        }
+
+
+    }
+}
